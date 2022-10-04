688cdc999633a96adc124faa705e82038b281727
dearhui <dearhui@gmail.com>
2022-10-04 15:36:34 +0800

    fix crash on alert

--- Sources/SwiftUIWebView/SwiftUIWebView.swift
+++ Sources/SwiftUIWebView/SwiftUIWebView.swift
@@ -196,8 +196,8 @@ extension WebViewCoordinator: WKUIDelegate {
             completionHandler(false)
         }))
         
-        if let rootVC = UIApplication.shared.windows.first?.rootViewController {
-            rootVC.present(alertController, animated: true, completion: nil)
+        if let controller = topMostViewController() {
+            controller.present(alertController, animated: true, completion: nil)
         }
     }
     
@@ -208,8 +208,8 @@ extension WebViewCoordinator: WKUIDelegate {
             completionHandler()
         }))
         
-        if let rootVC = UIApplication.shared.windows.first?.rootViewController {
-            rootVC.present(alertController, animated: true, completion: nil)
+        if let controller = topMostViewController() {
+            controller.present(alertController, animated: true, completion: nil)
         }
     }
 }
@@ -495,6 +495,37 @@ struct WebViewTest: View {
             }
         }
     }
+}
+
+private func topMostViewController() -> UIViewController? {
+    guard let rootController = keyWindow()?.rootViewController else {
+        return nil
+    }
+    return topMostViewController(for: rootController)
+}
+
+private func keyWindow() -> UIWindow? {
+    return UIApplication.shared.connectedScenes
+        .filter {$0.activationState == .foregroundActive}
+        .compactMap {$0 as? UIWindowScene}
+        .first?.windows.filter {$0.isKeyWindow}.first
+}
+
+private func topMostViewController(for controller: UIViewController) -> UIViewController {
+    if let presentedController = controller.presentedViewController {
+        return topMostViewController(for: presentedController)
+    } else if let navigationController = controller as? UINavigationController {
+        guard let topController = navigationController.topViewController else {
+            return navigationController
+        }
+        return topMostViewController(for: topController)
+    } else if let tabController = controller as? UITabBarController {
+        guard let topController = tabController.selectedViewController else {
+            return tabController
+        }
+        return topMostViewController(for: topController)
+    }
+    return controller
 }
 
 struct WebView_Previews: PreviewProvider {
